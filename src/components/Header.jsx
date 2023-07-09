65bd325aa159edbb99e0cf4da8e7efcb70ad49f7
Mishragini <mishragini11.com>
2023-07-09 18:12:12 +0530

    added intial files

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+function Header(){
+    return <h1 className="header"><TextSnippetIcon /> Keeper</h1>;
+}
+
+export default Header;
